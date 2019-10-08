9b955307c90583b11d99f29d5f8d7ab1913b4df4
Wei <qiwei1985915@163.com>
2019-10-08 23:16:39 +0800

    Add files via upload

--- lesson5/test.c
+++ lesson5/test.c
@@ -1,6 +1,23 @@
+//
+//  main.c
+//  example1_3
+//
+//  Created by TI on 2018/10/23.
+//  Copyright © 2018年 TI. All rights reserved.
+//
+
 #include <stdio.h>
 
-int main()
+//主函数
+int main()                  // 定义主函数
 {
+    int a;                   // 定义变量a，b
+    printf("是否心仪: \n");   // 输入提示
+    scanf("%d", &a);        // 输入变量a和b的值
+    if(a==1)
+        printf("聊聊天文地理、琴棋书画、斧钺钩叉、煎炒烹炸！\n", c);
+    else
+        printf("老铁，咱们聊聊C语言程序设计，基本数据类型啥的！\n", c);
 
+    return 0;
 }
---
+++ lesson5/test3.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d, %d, %d", &a, &b, &c:);
+
+    if (a>b) {
+        if(a>c)
+            printf("max = %d", a);/* code */
+        else
+            printf("max = %d", c);/* code */
+
+    }
+
+    if(a<b){
+        if(b>c)
+            printf("max = %d", b);
+        else
+            printf("max = %d", c:);
+    }
+
+    return 0;
+}
